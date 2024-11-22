c2ce9cdf9e44149291c653f4205b6a36f5bf2927
conradotulio <148455360+conradotulio@users.noreply.github.com>
2024-11-22 08:34:54 -0300

    ..

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que se ensina no curso de programação?',
+    ' visa fornecer conhecimento teórico e prático sobre linguagens de programação, desenvolvimento de software, solução de problemas e lógica de programação.'
+)
+
+criaCartao(
+    'Programação',
+    'Quais são as vantagens da programação?',
+    'criar soluções para problemas através do desenvolvimento de instruções e algoritmos que podem ser executados por computadores.'
+)
+
+criaCartao(
+    'Programação',
+    'Qual é o objetivo da programação?',
+    'escreve códigos simples e complexos por meio de linguagens de programação como HTML, Javascript, Python ou C++.'
+)
+
+criaCartao(
+    'Programação',
+    'Qual a linguagem da programação?',
+    'JavaScript,, Python, Java, PHP, CSS, C#, C++ e C..'
+)
